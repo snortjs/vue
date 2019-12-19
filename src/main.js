404f847981f1dd9d5abf759b8a1d92d2697ea56d
deniszulic <35138382+deniszulic@users.noreply.github.com>
2019-12-19 11:02:59 +0100

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import firebase from 'firebase'
+
 
 Vue.config.productionTip = false
+firebase.initializeApp ({
+  apiKey: "AIzaSyAvsC39Pau7ScnNw0o39aysHpgI8PKm_uQ",
+    authDomain: "nastava-3ae81.firebaseapp.com",
+    databaseURL: "https://nastava-3ae81.firebaseio.com",
+    projectId: "nastava-3ae81",
+    storageBucket: "nastava-3ae81.appspot.com",
+    messagingSenderId: "49973605599",
+    appId: "1:49973605599:web:761e904bd860d8460fe56a",
+    measurementId: "G-69T10SP430"
+});
 
 new Vue({
   router,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,106 @@
 <template>
-<div class="container">
-<div class="jumbotron">
-  <h1 class="display-4">Hello, world!</h1>
-  <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-  <hr class="my-4">
-  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-  <p class="lead">
-    <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
-  </p>
-</div></div>
+  <div class="row">
+    <div class="col-8">
+
+      <div v-if="authenticated">
+         <form @submit.prevent="postNewImage" class="form-inline mb-5">
+ <div class="form-group">
+ <label for="imageUrl">Image URL</label>
+ <input v-model="newImageUrl" type="text" class="form-control ml-2"
+id="imageUrl" placeholder="Enter the image URL">
+ </div>
+ <button type="submit" class="btn btn-primary ml-2">Post image</button>
+ 
+ </form>
+ <div>
+<input type="file" id="files" name="files[]" multiple />
+<button v-on:click="uploadFile()">Pohrani</button>
+</div>
+<button v-on:click="ok()">Prikazi</button>
+<InstagramCard :key="card.id" :info="card" v-for="card in filteredCards" />
+      </div>
+
+    </div>
+    <div class="col-4">
+      Trazimo sljedece: {{ searchTerm }}
+    </div>
+  </div>
 </template>
+
+<script>
+import InstagramCard from '@/components/InstagramCard.vue'
+import store from '@/store.js'
+import firebase from 'firebase';
+import * as firebase1 from 'firebase/app';
+
+export default {
+  data () {
+    return store;
+  
+  },
+  computed: {
+    filteredCards () {
+      // let filtered = []
+      // for(let card of this.cards) {
+      //   if (card.title.includes(this.searchTerm)) {
+      //     filtered.push(card)
+      //   }
+      // }
+      // return filtered;
+      return this.cards.filter(card => card.title.includes(this.searchTerm));
+    }
+  },
+  name: 'home',
+  components: {
+    InstagramCard
+  },
+ methods: {
+ postNewImage() {
+db.collection("posts").add({
+ url: this.newImageUrl,
+ email: this.userEmail,
+ posted_at: Date.now()
+ })
+ },
+   uploadFile(){
+      var storage = firebase.storage();
+
+      var file = document.getElementById("files").files[0];
+      console.log(file);
+      var storageRef = firebase.storage().ref();
+      var thisRef = storageRef.child("oof/"+file.name);
+      thisRef.put(file).then(function(snapshot) {
+      console.log('Uploadano');
+      });
+    },
+  ok(){
+    var storageRef = firebase1.storage().ref("oof");
+storageRef.listAll().then(function(result) {
+  result.items.forEach(function(imageRef) {
+    displayImage(imageRef);
+  });
+}).catch(function(error) {
+});
+
+function displayImage(imageRef) {
+  imageRef.getDownloadURL().then(function(url) {
+    console.log(url)
+  }).catch(function(error) {
+  });
+}
+  }
+}
+}
+</script>
+
+<style lang="scss">
+
+.card-body {
+  padding: 0px;
+}
+
+img:hover {
+  cursor: pointer;
+}
+
+</style>
